273f4b71415a04b0e65dd38181fe4f9cf666f915
Sai Suraj Prathigadapa <saisurajprathigadapa@gmail.com>
2023-02-24 14:28:03 +0530

    implemented upload functionality

--- src/main/java/com/userservice/controller/UserController.java
+++ src/main/java/com/userservice/controller/UserController.java
@@ -1,41 +1,41 @@
 package com.userservice.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.userservice.dto.Root;
 import com.userservice.dto.UserDetailsDto;
 import com.userservice.exceptions.ApiResponse;
 import com.userservice.service.ImageService;
 import com.userservice.service.UserService;
-import com.userservice.util.UserUtils;	
+import com.userservice.util.UserUtils;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private ImageService imageService;
-	
 
 	@GetMapping("/user")
 	public ResponseEntity<UserDetailsDto> getUserProfile() throws Exception {
 		return new ResponseEntity<>(userService.getUserProfile(), HttpStatus.OK);
 	}
-	
-	@GetMapping("/user/images")
-	public ResponseEntity<List<String>> getAllImages() throws Exception {
-		return new ResponseEntity<List<String>>(imageService.getAllImages(), HttpStatus.OK);
-		
+
+	@PostMapping("/user/images")
+	public ResponseEntity<Root> uploadImage(@RequestParam("file") MultipartFile file) throws Exception {
+		return new ResponseEntity<Root>(imageService.uploadImage(file), HttpStatus.CREATED);
+
 	}
 
 	@GetMapping("/user/images/{imageId}")
@@ -47,9 +47,12 @@ public class UserController {
 	public ResponseEntity<ApiResponse> deleteImage(@PathVariable("imageId") String imageId) {
 		try {
 			imageService.deleteImage(imageId);
-			return new ResponseEntity<>(UserUtils.getProperApiResponse(HttpStatus.OK.value(), UserUtils.DELETION_SUCCESSFULL), HttpStatus.OK);
+			return new ResponseEntity<>(
+					UserUtils.getProperApiResponse(HttpStatus.OK.value(), UserUtils.DELETION_SUCCESSFULL),
+					HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(UserUtils.getProperApiResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), UserUtils.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(UserUtils.getProperApiResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					UserUtils.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	}
--- src/main/java/com/userservice/service/ImageService.java
+++ src/main/java/com/userservice/service/ImageService.java
@@ -1,5 +1,6 @@
 package com.userservice.service;
 
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,39 +8,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.userservice.dto.Root;
 import com.userservice.exceptions.ImageNotFoundException;
-import com.userservice.exceptions.RestTemplateResponseErrorHandler;
 import com.userservice.util.UserUtils;
 
 @Service
 public class ImageService {
 
-	@Value("${imgur.clientid}")
-	private String IMGUR_CLIENT_ID;
+	@Value("${imgur.token}")
+	private String token;
 	
 	@Value("${imgur.baseurl}")
-	private String BASE_URL;
+	private String baseUrl;
 	
+	@Autowired
 	private RestTemplate restTemplate;
 	
 	private static final Logger logger = LoggerFactory.getLogger(ImageService.class);
+    
+    public Root uploadImage(MultipartFile file) throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		headers.add("Authorization", "Client-ID " + token);
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		Base64.Encoder encoder = Base64.getEncoder();
+		body.add("image", encoder.encode(file.getBytes()));
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+		try {
+			return restTemplate.postForObject(baseUrl + "upload", requestEntity, Root.class);
+		}
+		catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 
-    @Autowired
-    public ImageService(RestTemplateBuilder restTemplateBuilder) {
-        restTemplate = restTemplateBuilder
-          .errorHandler(new RestTemplateResponseErrorHandler())
-          .build();
-    }
+	}
     
 	/*
 	 * 	Retrieves Principal from current Security Context if exists
@@ -48,13 +62,20 @@ public class ImageService {
 	 *  @throws ImageNotFoundException if image doesn't exist
 	 *  @throws PricipalNotFoundException if Principal doesn't exist
 	 */
+
 	
 	public Root getImage(String imageId) throws Exception {
 		String username = UserUtils.getCurrentPrincipal().getUsername();
+		ResponseEntity<Root> response = null;
 	    HttpHeaders headers = new HttpHeaders();
-	    headers.add("Authorization", "Client-ID " + IMGUR_CLIENT_ID);
+	    headers.add("Authorization", "Client-ID " + token);
 	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-	    ResponseEntity<Root> response = restTemplate.exchange(BASE_URL+username+"/image/"+imageId, HttpMethod.GET, entity, Root.class);
+	    try {
+	    	response = restTemplate.exchange(baseUrl + "account/" +username + "/image/" + imageId, HttpMethod.GET, entity, Root.class);
+	    }
+	    catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 	    if(response.hasBody()) {
 	    	if(response.getBody().getData() != null) {
 	    		logger.info("Successully retrieved the image with ID : "+imageId);
@@ -79,10 +100,10 @@ public class ImageService {
 	public void deleteImage(String imageId) throws Exception {
 		String username = UserUtils.getCurrentPrincipal().getUsername();
 	    HttpHeaders headers = new HttpHeaders();
-	    headers.add("Authorization", "Client-ID " + IMGUR_CLIENT_ID);
+	    headers.add("Authorization", "Client-ID " + token);
 	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 	    try {
-	    	restTemplate.delete(BASE_URL+username+"/image/"+imageId, entity);
+	    	restTemplate.delete(baseUrl+ "account/" + username + "/image/" +imageId, entity);
 	    }
 	    catch (Exception e) {
 			throw new Exception(UserUtils.INTERNAL_SERVER_ERROR);
@@ -98,11 +119,16 @@ public class ImageService {
 	
 	public List<String> getAllImages() throws Exception {
 		String username = UserUtils.getCurrentPrincipal().getUsername();
+		ResponseEntity<List<Root>> response = null;
 	    HttpHeaders headers = new HttpHeaders();
-	    headers.add("Authorization", "Client-ID " + IMGUR_CLIENT_ID);
+	    headers.add("Authorization", "Client-ID " + token);
 	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-	    ResponseEntity<List<Root>> response = restTemplate.exchange(BASE_URL+username+"/images/", HttpMethod.GET, entity, new ParameterizedTypeReference<List<Root>>() {
-		});
+	    try {
+	    	response = restTemplate.exchange(baseUrl+username+"/images/", HttpMethod.GET, entity, new ParameterizedTypeReference<List<Root>>() {});
+	    }
+	    catch (Exception e) {
+			new Exception(e.getMessage());
+		}
 	    if(response.hasBody()) {
 	    	if(response.getBody().size() > 0) {
 	    		logger.info("Successully retrieved the images for username : "+username);
